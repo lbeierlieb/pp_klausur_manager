bf702a550ee9292b415430cc83589ebdd9aa2d06
Lukas Beierlieb <lukasbrlb.lb@gmail.com>
2025-04-12 12:35:10 +0200

    nixosTest: add config shared between machines

--- nix/test.nix
+++ nix/test.nix
@@ -1,10 +1,22 @@
 { pkgs }:
+let
+  common = {config, pkgs, ... }: {
+    users.users.nixos = {
+      isNormalUser  = true;
+      home  = "/home/nixos";
+      extraGroups  = [ "wheel" ];
+      initialPassword = "nixos";
+    };
+  };
+in
 pkgs.nixosTest {
   name = "test";
   nodes.control-machine = { config, pkgs, ... }: {
+    imports = [ common ];
     system.stateVersion = "24.11";
   };
   nodes.client1 = { config, pkgs, ... }: {
+    imports = [ common ];
     system.stateVersion = "24.11";
   };
   testScript = "";
