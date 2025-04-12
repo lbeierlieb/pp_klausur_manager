0bf87ecc280e4b875e40f74abb6461dc3093a924
Lukas Beierlieb <lukasbrlb.lb@gmail.com>
2025-04-12 12:37:25 +0200

    nixosTest: move stateVersion to common config

--- nix/test.nix
+++ nix/test.nix
@@ -7,17 +7,16 @@ let
       extraGroups  = [ "wheel" ];
       initialPassword = "nixos";
     };
+    system.stateVersion = "24.11";
   };
 in
 pkgs.nixosTest {
   name = "test";
   nodes.control-machine = { config, pkgs, ... }: {
     imports = [ common ];
-    system.stateVersion = "24.11";
   };
   nodes.client1 = { config, pkgs, ... }: {
     imports = [ common ];
-    system.stateVersion = "24.11";
   };
   testScript = "";
 }
