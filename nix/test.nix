69c7ebcce1cdb11369dce0ca1e493799020ce801
Lukas Beierlieb <lukasbrlb.lb@gmail.com>
2025-04-14 23:19:14 +0200

    nixosTest: start three clients

--- nix/test.nix
+++ nix/test.nix
@@ -3,6 +3,8 @@ pkgs.nixosTest {
   name = "test";
   nodes.control-machine = import ./config_controlmachine.nix { inherit pp_klausur_manager; };
   nodes.client1 = import ./config_client.nix;
+  nodes.client2 = import ./config_client.nix;
+  nodes.client3 = import ./config_client.nix;
   testScript = ''
     control_machine.start()
     control_machine.wait_for_unit("nfs-server.service")
