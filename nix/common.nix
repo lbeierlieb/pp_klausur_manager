b2caa6eb4d286c7d54eb5976088ba83822906fb5
Lukas Beierlieb <lukasbrlb.lb@gmail.com>
2025-04-15 07:14:48 +0200

    nixpkgs-fmt

--- nix/common.nix
+++ nix/common.nix
@@ -1,11 +1,11 @@
 { ... }: {
-    virtualisation.memorySize = 2048;
-    virtualisation.cores = 2;
-    users.users.nixos = {
-      isNormalUser = true;
-      home = "/home/nixos";
-      initialPassword = "nixos";
-    };
-    networking.firewall.enable = false;
-    system.stateVersion = "24.11";
-  }
+  virtualisation.memorySize = 2048;
+  virtualisation.cores = 2;
+  users.users.nixos = {
+    isNormalUser = true;
+    home = "/home/nixos";
+    initialPassword = "nixos";
+  };
+  networking.firewall.enable = false;
+  system.stateVersion = "24.11";
+}
